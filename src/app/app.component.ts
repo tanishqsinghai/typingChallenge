7bf40f18eadb063788fa6fe8fdcd133707eb1fc4
tanishq <tanishqsinghai11@gmail.com>
2020-10-15 17:27:11 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
-
+import {lorem} from 'faker';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'typingChallenge';
+  randomText = lorem.sentence();
+  text='';
+
+  onInput(value : string)
+  {
+    this.text=value;
+  }
+  compare(letter : string,text : string)
+  {
+    if(!text)
+      return 'pending';
+    if(letter===text)
+      return 'correct';
+    return 'incorrect';
+  }
 }
